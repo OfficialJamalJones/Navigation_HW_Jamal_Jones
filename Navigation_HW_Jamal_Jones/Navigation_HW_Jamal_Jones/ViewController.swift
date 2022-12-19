fd3689419503fd5c114b343bbff85613b8ec37e9
Jamal Jones <74991730+OfficialJamalJones@users.noreply.github.com>
2022-12-19 12:51:29 -0500

    Add files via upload

---
+++ Navigation_HW_Jamal_Jones/Navigation_HW_Jamal_Jones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation_HW_Jamal_Jones
+//
+//  Created by Consultant on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Navigation_HW_Jamal_Jones/Navigation_HW_Jamal_JonesUITests/Navigation_HW_Jamal_JonesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Navigation_HW_Jamal_JonesUITestsLaunchTests.swift
+//  Navigation_HW_Jamal_JonesUITests
+//
+//  Created by Consultant on 12/19/22.
+//
+
+import XCTest
+
+final class Navigation_HW_Jamal_JonesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
