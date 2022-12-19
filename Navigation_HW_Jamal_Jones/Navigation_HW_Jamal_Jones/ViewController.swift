8f486b44effff3072164e9424099f14b5371eede
OfficialJamalJones <jamalamanjones@gmail.com>
2022-12-19 12:54:19 -0500

    Added Comments

--- Navigation_HW_Jamal_Jones/Navigation_HW_Jamal_Jones/ViewController.swift
+++ Navigation_HW_Jamal_Jones/Navigation_HW_Jamal_Jones/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //HW: Create a basic application with a table/Collection view, showing some basic data. When selecting a cell, it will navigate them over to a detail view. Data, constraints, and form of navigation will be up to the individual
+    
+    
+//HW: Create MVC flowchart.
 
     override func viewDidLoad() {
         super.viewDidLoad()
